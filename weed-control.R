8b0fa347a48cc44896a72009c292756a7c605d62
Dan Dauwalter <ddauwalter@tu.org>
2021-11-18 11:29:39 -0700

    Updating rmd and r files

--- weed-control.R
+++ weed-control.R
@@ -3,16 +3,58 @@ attach(DK)
 
 library(lme4)
 library(MuMIn)
-library(visreg)
 library(ggplot2)
 library(ggpubr)
 library(lattice)
+library(visreg)
 
 View(DK)
 
 ##scale n center data due to error in reduced model
 DK.s<-DK
 DK.s[,names(DK) %in% c("JulDaySurv","VegRemoved","Visibility")]<-scale(DK[,names(DK) %in% c("JulDaySurv","VegRemoved","Visibility")])
+
+## summarize
+library(tidyverse)
+DK2<-DK %>%
+  group_by(Site,DepthCat,SurvCat,Type) %>%
+  summarize(mean=mean(ALL), SD=sd(ALL), n=n())
+
+library(data.table)
+
+setDT(DK2)
+DK.sum<-dcast(DK2, Site+DepthCat+SurvCat ~ Type, value.var = "mean")
+(DK.sum$TCRatio<-DK.sum$Treat/DK.sum$Control)
+mean(DK.sum$TCRatio)
+sd(DK.sum$TCRatio)
+
+shapiro.test(DK.sum$TCRatio)
+library("ggpubr")
+ggqqplot(DK.sum$TCRatio, ylab = "Treat:Control Ratio",
+         ggtheme = theme_minimal())
+#both suggest at least one outlier, that is removed. analyses repeated [-3]
+shapiro.test(DK.sum$TCRatio[-3])
+
+ggqqplot(DK.sum$TCRatio[-3], ylab = "Treat:Control Ratio",
+         ggtheme = theme_minimal())
+#mean and SD of T:C Ratio with outlier removed
+mean(DK.sum$TCRatio[-3])
+sd(DK.sum$TCRatio[-3])
+
+#one sample t-test to show if H0=1
+t.test(DK.sum$TCRatio[-3], mu = 1)
+
+#t-test to compare ratios between 11 and 22 ft depths. outlier removed
+var.test(TCRatio ~ DepthCat, data = DK.sum[-3,])
+t.test(DK.sum[DepthCat=="Eleven"]$TCRatio, DK.sum[DepthCat=="TwentyTwo"][-1]$TCRatio, var.equal = FALSE)
+mean(DK.sum[DepthCat=="Eleven"]$TCRatio)
+sd(DK.sum[DepthCat=="Eleven"]$TCRatio)
+mean(DK.sum[DepthCat=="TwentyTwo"]$TCRatio[-1])
+sd(DK.sum[DepthCat=="TwentyTwo"]$TCRatio[-1])
+
+t.test(DK.sum[DepthCat=="Eleven"]$TCRatio, mu = 1)
+
+t.test(DK.sum[DepthCat=="TwentyTwo"]$TCRatio[-1], mu = 1)
 
 ## Variable Correlations
 corr<-cor(DK[sapply(DK,is.numeric)])
@@ -41,8 +83,9 @@ AIC(gm1, gm1.site.reff, gm2.site.surv.reff,gm3.site.surv_reff)#AIC,  suggests ra
 
 ## Global model
 Global.mod<-glmer(ALL~JulDaySurv+Type*DepthCat+Visibility+(1|Site)+(1|SurvCat),
-                data=DK.s,family=poisson,nAGQ=0, na.action="na.fail")  #Insert response
+                data=DK.s,family=poisson, na.action="na.fail")  #Insert response
 summary(Global.mod)
+plot(Global.mod)
 
 mod.sel<-dredge(Global.mod,rank="AICc", REML = FALSE, fixed=c("Type"))#, subset=!("Type" && "VegRemoved"))
 (top.mods<-subset(mod.sel,subset = delta<4))
@@ -55,17 +98,15 @@ summary(avgm)
 r.mod<-glmer(ALL~Type*DepthCat+(1|Site)+(1|SurvCat), 
              data=DK.s, family=poisson,na.action="na.fail")  
 summary(r.mod)
-
+plot(r.mod)
 ## Plots of species/group vs. treatment
 
-visreg(r.mod, "Type", by="DepthCat",type='conditional',ylab="Number of fish (all species)")
+visreg(r.mod, "Type", by="DepthCat",
+       type='conditional',ylab="log(Number of Fish)")
 
 randoms <- ranef(r.mod)
-tiff("randeffs.tiff",width=7,height=10,units='in',res=300)
-par(mfrow=c(1,2),mar=c(2,2,2,2),oma=c(3,3,1,1))
 (srvy.rand.eff<-dotplot(randoms)$SurvCat)
 (site.rand.eff<-dotplot(randoms)$Site)
-dev.off()
 #rand.eff.fig<-ggarrange(srvy_site,site, labels = c("A)", "B)"),
                   #ncol = 1, nrow = 2, heights=c(1,1), common.legend = TRUE)
 #rand.eff.fig
